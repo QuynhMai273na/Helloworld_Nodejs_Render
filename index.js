e8541ef3b94c104619688cf53142581a05639bea
Nguyễn Thị Quỳnh Mai <147372843+QuynhMai273na@users.noreply.github.com>
2024-09-13 15:48:00 +0700

    add "start" into "scripts" in pkg.json

---
+++ index.js
@@ -0,0 +1,29 @@
+// Hello world project thuan node js
+const http = require('http');//node.js
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+// Hello world voi Express
+// const express = require('express');
+
+// const app = express();
+// const port = 8080;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World! Welcome to my server!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
