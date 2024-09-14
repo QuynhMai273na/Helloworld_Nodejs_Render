2b885c67c00f030f02f08d993933a99086c40f46
Nguyễn Thị Quỳnh Mai <147372843+QuynhMai273na@users.noreply.github.com>
2024-09-14 20:23:25 +0700

    change express to nodejs

--- index.js
+++ index.js
@@ -27,14 +27,28 @@
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
+const http = require('http'); // Sử dụng thư viện http của Node.js
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3005; // Sử dụng biến môi trường PORT nếu có hoặc 3005
 
-app.get("/", (req, res) => res.type('html').send(html));
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.type('html').send(html);
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Lắng nghe trên cổng được chỉ định mà không cần hostname
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// app.get("/", (req, res) => res.type('html').send(html));
+
+// const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
